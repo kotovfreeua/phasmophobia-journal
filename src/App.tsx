d3153c1f560d5d529ce898526a9a32b0dfaabe83
Kotov <kotov.smtp@gmail.com>
2022-05-21 19:51:25 +0300

    🧹 cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <main className='flex justify-center items-center min-h-screen'>
+    <h1 className='text-3xl font-black text-stone-900'>
+      Phahsmophobia Journal
+    </h1>
+  </main>
+);
 
 export default App;
