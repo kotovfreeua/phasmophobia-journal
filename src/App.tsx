356156612da9286adbcffa36856b7bbaf83eebff
Kotov <kotov.smtp@gmail.com>
2022-05-22 01:00:30 +0300

    ⚛️ add created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,121 @@
-const App: React.FC = () => (
-  <main className='flex justify-center items-center min-h-screen'>
-    <h1 className='text-3xl font-black text-stone-900'>
-      Phahsmophobia Journal
-    </h1>
-  </main>
-);
+import { useState } from "react";
+import classNames from "./utils/classNames";
+import { ghostData, evidences, ghosts, GhostEnum, EvidenceEnum } from "./data";
+
+const App: React.FC = () => {
+  const [trueEvidences, setTrueEvidences] = useState<EvidenceEnum[]>([]);
+  const [falseEvidences, setFalseEvidences] = useState<EvidenceEnum[]>([]);
+  const [selectedGhost, setSelectedGhost] = useState<GhostEnum | null>(null);
+  const [falseGhosts, setFalseGhosts] = useState<GhostEnum[]>([]);
+
+  const handleEvidencesChange = (currentEvidence: EvidenceEnum) => () => {
+    if (trueEvidences.includes(currentEvidence)) {
+      setTrueEvidences(trueEvidences.filter((evidence) => evidence !== currentEvidence));
+      setFalseEvidences([...falseEvidences, currentEvidence]);
+      return;
+    }
+
+    if (falseEvidences.includes(currentEvidence)) {
+      setFalseEvidences(
+        falseEvidences.filter((evidence) => evidence !== currentEvidence)
+      );
+      return;
+    }
+
+    setTrueEvidences([...trueEvidences, currentEvidence]);
+  };
+
+  const handleGhostSelect = (currentGhost: GhostEnum) => () => {
+    if (!isPossibleGhost(currentGhost)) return;
+
+    if (selectedGhost === currentGhost) {
+      setSelectedGhost(null);
+      setFalseGhosts([...falseGhosts, currentGhost]);
+      return;
+    }
+
+    if (falseGhosts.includes(currentGhost)) {
+      setFalseGhosts(falseGhosts.filter((ghost) => ghost !== currentGhost));
+      return;
+    }
+
+    setSelectedGhost(currentGhost);
+  };
+
+  const isPossibleGhost = (ghostName: GhostEnum) =>
+    trueEvidences.every((evidence) => ghostData[ghostName].evidence.includes(evidence)) &&
+    !falseEvidences.some((evidence) => ghostData[ghostName].evidence.includes(evidence));
+
+  return (
+    <main className='flex flex-col justify-center items-center min-h-screen'>
+      <h1 className='text-2xl md:text-3xl font-black text-stone-900 mt-12'>
+        Phasmophobia Journal
+      </h1>
+
+      <div className='max-w-md w-full px-4 my-12'>
+        <span className='text-xl font-bold'>Evidence</span>
+
+        <hr className='border-b border-stone-900 my-4' />
+
+        <div className='flex flex-wrap gap-y-4 md:gap-4'>
+          {evidences.map((evidence) => (
+            <div key={evidence} className='flex w-36 grow'>
+              <label className='flex items-center gap-2 md:gap-3 cursor-pointer'>
+                <input
+                  type='checkbox'
+                  value={evidence}
+                  className='appearance-none w-4 h-4 md:w-6 md:h-6 border-2 md:border-[3px] checked:bg-stone-900 border-stone-900 cursor-pointer'
+                  onChange={handleEvidencesChange(evidence)}
+                  checked={trueEvidences.includes(evidence)}
+                />
+                <span
+                  className={classNames(
+                    "md:text-lg font-medium whitespace-nowrap select-none",
+                    falseEvidences.includes(evidence) && "line-through"
+                  )}
+                >
+                  {evidence}
+                </span>
+              </label>
+            </div>
+          ))}
+        </div>
+
+        <hr className='border-b border-stone-900 my-4' />
+
+        <p className='md:text-lg text-center mb-4'>
+          The ghost type could still be one of these.
+        </p>
+
+        <div className='grid grid-cols-3 gap-3 text-center'>
+          {ghosts.map((ghost) => (
+            <span
+              key={ghost}
+              className={classNames(
+                "md:text-lg font-medium border-2 border-transparent rounded-[45%] cursor-pointer select-none",
+                selectedGhost === ghost && "!border-stone-900",
+                falseGhosts.includes(ghost) && "line-through",
+                !isPossibleGhost(ghost) && "opacity-20"
+              )}
+              onClick={handleGhostSelect(ghost)}
+            >
+              {ghost}
+            </span>
+          ))}
+        </div>
+
+        <p className='md:text-lg text-center my-4 max-w-xs mx-auto'>
+          Using the evidence were found, we believe the ghost is a
+        </p>
+
+        <div className='flex justify-center items-center'>
+          <span className='text-3xl p-2 md:p-4'>
+            {selectedGhost || "Not yet discovered"}
+          </span>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/data/index.ts
@@ -0,0 +1,230 @@
+export enum GhostEnum {
+  BANSHEE = "Banshee",
+  DEMON = "Demon",
+  GORYO = "Goryo",
+  HANTU = "Hantu",
+  JINN = "Jinn",
+  MARE = "Mare",
+  MYLING = "Myling",
+  OBAKE = "Obake",
+  ONI = "Oni",
+  ONRYO = "Onryo",
+  PHANTOM = "Phantom",
+  POLTERGEIST = "Poltergeist",
+  RAIJU = "Raiju",
+  REVENANT = "Revenant",
+  SHADE = "Shade",
+  SPIRIT = "Spirit",
+  THE_MIMIC = "The Mimic",
+  THE_TWINS = "The Twins",
+  WRAITH = "Wraith",
+  YOKAI = "Yokai",
+  YUREI = "Yurei"
+}
+
+export enum EvidenceEnum {
+  EEMF_LEVEL_5 = "EEMF Level 5",
+  DOTS_PROJECTOR = "D.O.T.S Projector",
+  FINGERPRINTS = "Fingerprints",
+  GHOST_ORB = "Ghost Orb",
+  GHOST_WRITING = "Ghost Writing",
+  SPIRIT_BOX = "Spirit Box",
+  FREEZING_TEMPERATURES = "Freezing Temperatures"
+}
+
+export const evidences = Object.values(EvidenceEnum);
+
+export const ghosts = Object.values(GhostEnum);
+
+export const ghostData = {
+  [GhostEnum.BANSHEE]: {
+    strength: "Will target only one player at a time",
+    weakness: "Has a distinctive wail on the Parabolic Microphone.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.FINGERPRINTS
+    ]
+  },
+  [GhostEnum.DEMON]: {
+    strength: "Can initiate hunts more often",
+    weakness: "Crucifix effectiveness is increased to 5m against one.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.FINGERPRINTS,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  },
+  [GhostEnum.GORYO]: {
+    strength:
+      "Can only be seen interacting with D.O.T.S. through a camera when nobody is nearby",
+    weakness: "Tends to wander away less from its ghost room.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.FINGERPRINTS
+    ]
+  },
+  [GhostEnum.HANTU]: {
+    strength: "Lower temperatures allow the Hantu to move faster",
+    weakness: "Warmer areas slow the Hantu's movement.",
+    evidence: [
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.FINGERPRINTS,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  },
+  [GhostEnum.JINN]: {
+    strength: "Travels at faster speeds if its victim is far away",
+    weakness: "Cannot use its ability if the site's fuse box is off.",
+    evidence: [
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.FINGERPRINTS,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  },
+  [GhostEnum.MARE]: {
+    strength: "Has an increased chance to attack in the dark",
+    weakness: "Turning the lights on will reduce the chance of an attack.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.MYLING]: {
+    strength: "Has quieter footsteps during a hunt",
+    weakness: "Produces paranormal sounds more frequently.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.FINGERPRINTS
+    ]
+  },
+  [GhostEnum.OBAKE]: {
+    strength: "May leave fingerprints that disappear quicker",
+    weakness: "Has a small chance of leaving six-fingered handprints.",
+    evidence: [
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.FINGERPRINTS
+    ]
+  },
+  [GhostEnum.ONI]: {
+    strength: "Increased activity and ghost events",
+    weakness: "An Oni's increased activity makes them easier to find.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  },
+  [GhostEnum.ONRYO]: {
+    strength: "A flame extinguishing can cause an Onryo to attack",
+    weakness: "The presence of flames reduces the Onryo's ability to attack.",
+    evidence: [
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.FREEZING_TEMPERATURES,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.PHANTOM]: {
+    strength: "Looking at a Phantom will lower the player's sanity considerably",
+    weakness: "Taking a photo of the Phantom will cause it to briefly disappear.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.FINGERPRINTS,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.POLTERGEIST]: {
+    strength: "Capable of throwing multiple objects at once",
+    weakness: "Becomes powerless with no throwables nearby.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.FINGERPRINTS,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.RAIJU]: {
+    strength: "Moves faster near electrical devices",
+    weakness: "Disrupts electronic equipment from further away when it hunts.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.GHOST_ORB
+    ]
+  },
+  [GhostEnum.REVENANT]: {
+    strength: "Can travel significantly faster if a player is spotted during a hunt",
+    weakness: "Moves very slowly when not chasing a player.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  },
+  [GhostEnum.SHADE]: {
+    strength: "Being shy makes it more difficult to locate and obtain evidence",
+    weakness: "Less likely to hunt if multiple people are nearby.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  },
+  [GhostEnum.SPIRIT]: {
+    strength: "None",
+    weakness: "Smudge sticks are more effective, preventing a hunt for longer.",
+    evidence: [
+      EvidenceEnum.GHOST_WRITING,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.THE_MIMIC]: {
+    strength: "Can mimic the abilities and traits of other ghosts",
+    weakness: "Will present Ghost Orbs as a secondary evidence.",
+    evidence: [
+      EvidenceEnum.FINGERPRINTS,
+      EvidenceEnum.FREEZING_TEMPERATURES,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.THE_TWINS]: {
+    strength: "Either Twin may start a hunt, though not at the same time",
+    weakness: "Will often interact with the environment at the same time",
+    evidence: [
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.FREEZING_TEMPERATURES,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.WRAITH]: {
+    strength: "Does not leave UV footprints after stepping in salt",
+    weakness: "Will become more active if it steps in salt.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.EEMF_LEVEL_5,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.YOKAI]: {
+    strength: "Talking near the Yokai will anger it, increasing the chance to attack",
+    weakness: "Can only hear voices close to it during a hunt.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.SPIRIT_BOX
+    ]
+  },
+  [GhostEnum.YUREI]: {
+    strength: "Has a stronger effect on sanity",
+    weakness: "Smudging the Yurei's ghost room will reduce how often it wanders.",
+    evidence: [
+      EvidenceEnum.DOTS_PROJECTOR,
+      EvidenceEnum.GHOST_ORB,
+      EvidenceEnum.FREEZING_TEMPERATURES
+    ]
+  }
+};
