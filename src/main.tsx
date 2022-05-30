a8353bd4cf1447f808904688f922d1b6cf8e4492
Kotov <kotov.smtp@gmail.com>
2022-05-30 20:53:26 +0300

    🚀 changed to vite

---
+++ src/main.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import App from "./App";
+
+import BookWrapper from "@/components/BookWrapper";
+import BookPage from "@/components/BookPage";
+import EvidencePage from "@/pages/EvidencePage";
+import GhostPage from "@/pages/GhostPage";
+import GhostTypesPage from "@/pages/GhostTypesPage";
+import NotFoundPage from "@/pages/NotFoundPage";
+
+import "@/styles/index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route
+            index
+            element={
+              <BookWrapper>
+                <GhostTypesPage />
+                <EvidencePage />
+              </BookWrapper>
+            }
+          />
+          <Route
+            path='info'
+            element={
+              <BookWrapper>
+                <BookPage />
+                <BookPage />
+              </BookWrapper>
+            }
+          />
+          <Route path='ghosts'>
+            <Route
+              path=':ghostType'
+              element={
+                <BookWrapper>
+                  <GhostTypesPage />
+                  <GhostPage />
+                </BookWrapper>
+              }
+            />
+            <Route
+              index
+              element={
+                <BookWrapper>
+                  <BookPage />
+                  <GhostTypesPage />
+                </BookWrapper>
+              }
+            />
+          </Route>
+
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
